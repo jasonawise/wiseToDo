d345ef6c0a8aef54d7f5d98f250068e5ebbc1c74
Jason A. Wise <jasonandrewwise@gmail.com>
2023-05-16 21:32:30 -0400

    Initial Commit

---
+++ wiseToDo/wiseToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiseToDoApp.swift
+//  wiseToDo
+//
+//  Created by Jason Wise on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct wiseToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
