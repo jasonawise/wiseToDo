58f823bac11d9c74155e708036cbdb83c89b4e27
Jason A. Wise <jasonandrewwise@gmail.com>
2023-05-19 18:58:27 -0400

    working on creating form to add new to do

--- wiseToDo/AddToDoView.swift
+++ wiseToDo/AddToDoView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct AddToDoView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var name: String = ""
+  @State private var selectedOption: String = "Option 1"
+  let options = ["to do", "in progress", "completed"]
+
+  var body: some View {
+    Form {
+      Section(header: Text("What Do you want to do?")) {
+        TextField("Todo ....", text: $name)
+      }
+
+      Section(header: Text("Status")) {
+        Picker(selection: $selectedOption, label: Text("Select an option")) {
+          ForEach(options, id: \.self) { option in
+            Text(option)
+          }
+        }
+      }
+      Button("Add") {
+        // action to add to do
+      }
     }
+  }
 }
 
 struct AddToDoView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddToDoView()
-    }
+  static var previews: some View {
+    AddToDoView()
+  }
 }
